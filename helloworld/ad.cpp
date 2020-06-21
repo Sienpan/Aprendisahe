4ba6eaa48493699faf5bfd98b4a75874197c7821
Sienpan <67244600+Sienpan@users.noreply.github.com>
2020-06-22 00:49:50 +0200

    Add files via upload

---
+++ helloworld/ad.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int cosa=1;
+int main(){
+cout<<"Hello World "<<cosa;
+return 0;
+}
---
+++ helloworld/pacomer.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int pollatiesa=420;
+int tuprima=2;
+int suma;
+int main(){
+    suma=pollatiesa*tuprima;
+    cout<<suma;
+    }
